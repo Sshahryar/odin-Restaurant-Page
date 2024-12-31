addbda1ece56a9c0471be8dea913946e2e6ffe13
Shahryar Hussain <123003299+Sshahryar@users.noreply.github.com>
2024-12-30 23:51:27 -0500

    Set up framework for tabbed browsing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import { loadHomePage } from './loadHomePage'
 
 console.log("Test");
+
+import { loadHomePage } from './home'; 
+import { loadMenuPage } from './menu'; 
+import { loadContactPage } from './contact'; 
+
+function clearContent() { 
+  const content = document.getElementById('content'); 
+  content.innerHTML = ''; 
+} 
+
+function switchTab(event) { 
+  clearContent(); if (event.target.id === 'home') loadHomePage(); 
+  if (event.target.id === 'menu') loadMenuPage(); 
+  if (event.target.id === 'contact') loadContactPage(); 
+} 
+
+document.getElementById('home').addEventListener('click', switchTab); 
+document.getElementById('menu').addEventListener('click', switchTab); 
+document.getElementById('contact').addEventListener('click', switchTab);
+
+loadHomePage();
